a0e7213d5af828ef29a94f41d4b130d3b8b21919
SneakyNieky <niekdommelen37@gmail.com>
2023-03-02 15:08:25 +0100

    Add project files.

---
+++ splinter/db.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace splinter
+{
+    class db
+    {
+        public static void connect()
+        {
+            string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Execute SQL commands here
+
+                connection.Close();
+            }
+
+        }
+    }
+}
