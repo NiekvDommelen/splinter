a5fc1cacfd602d81361cdf6fbf5962e5121436d9
SneakyNieky <niekdommelen37@gmail.com>
2023-03-03 01:00:32 +0100

    created db class

--- splinter/db.cs
+++ splinter/db.cs
@@ -1,28 +1,111 @@
 ﻿using System;
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Data;
+using System.Xml.Linq;
 
 namespace splinter
 {
     class db
     {
-        public static void connect()
-        {
-            string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+        public static string connectionString = "server=127.0.0.1;user id=root;password=niekdommelen;database=splinter";
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+        public static DataView getusers()
+        {
+
+            
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM `users`";
+                    var dataAdapter = new MySqlDataAdapter(sql, connection);
+                    var ds = new DataSet();
+                    dataAdapter.Fill(ds);
+
+
+                    connection.Close();
+                    return ds.Tables[0].DefaultView;
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message); 
+                return null;
+            }
+
+        }
+
+        public static DataView getposts()
+        {
+
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM `posts`";
+                    var dataAdapter = new MySqlDataAdapter(sql, connection);
+                    var ds = new DataSet();
+                    dataAdapter.Fill(ds);
+
+
+                    connection.Close();
+                    return ds.Tables[0].DefaultView;
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+        }
+
+        public static void createUser(string name, string username, string password)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
+                string sql = $"INSERT INTO users (name,username,password) VALUES ('{name}', '{username}', '{password}');";
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                var result = command.ExecuteNonQuery();
+                MessageBox.Show(result.ToString());
+                connection.Close();
+               
+            }
+        }
 
-                // Execute SQL commands here
+        public static void post( string author, string authorid, string title,string content)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
 
+                connection.Open();
+                string sql = $"INSERT INTO posts (author,authorid,title,content) VALUES ('{author}', '{authorid}', '{title}', '{content}');";
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                var result = command.ExecuteNonQuery();
+                MessageBox.Show("posted!!");
                 connection.Close();
             }
 
+        }
+
+        public static void login(string username, string password)
+        {
+            string sql = $"SELECT username, 'password' FROM `users` WHERE username = '{username}';";
         }
     }
 }
