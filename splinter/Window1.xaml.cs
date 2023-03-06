95819d11b5684c3d4a392700156d00356ca5d455
SneakyNieky <niekdommelen37@gmail.com>
2023-03-06 15:57:19 +0100

    added functionality to add files(images) to a post

--- splinter/Window1.xaml.cs
+++ splinter/Window1.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 using Org.BouncyCastle.Bcpg;
+using splinter;
 
 namespace splinter
 {
@@ -23,6 +25,7 @@ namespace splinter
         private int userid;
         private string name;
         private string username;
+        private string base64Image = null;
         public Window1(int _userid, string _name, string _username)
         {
             InitializeComponent();
@@ -47,9 +50,34 @@ namespace splinter
             string title = inptitle.Text;
             string content = inpcontent.Text;
             MessageBox.Show(userid.ToString()+ name + username);
-            db.post(username, userid, title, content);
+            db.post(username, userid, title, content, base64Image);
+            base64Image = null;
 
             getdata();
+        }
+
+        private void upload_Img_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".png";
+            fileDialog.Filter = "Image Files(*.PNG;*.JPG;*.GIF)|*.PNG;*.JPG;*.GIF|All files (*.*)|*.*";
+            fileDialog.FilterIndex = 2;
+            fileDialog.RestoreDirectory = true;
+             
+
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                //Get the path of specified file
+                string filePath = fileDialog.FileName;
+                imgpost.Source = new BitmapImage(new Uri(filePath));
+                base64Image = Converter.ToBase64(filePath);
+
+
+
+
+
+            }
         }
     }
 }
