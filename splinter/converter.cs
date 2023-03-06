dda5ff9e20e2ae8b4b764d5a8aa2f8a7bd0ccf1e
SneakyNieky <niekdommelen37@gmail.com>
2023-03-06 15:55:53 +0100

    created converter class to convert to and from base64

---
+++ splinter/converter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace splinter
+{
+    class Converter
+    {
+        public static string ToBase64(string filePath)
+        {
+            byte[] imageArray = File.ReadAllBytes(filePath);
+            string base64Image = Convert.ToBase64String(imageArray);
+            return base64Image;
+        }
+
+        public BitmapImage toImg(string base64Image)
+        {
+            byte[] bytes = Convert.FromBase64String(base64Image);
+            using (var ms = new MemoryStream(bytes))
+            {
+                // Create a new image object
+                var image = new BitmapImage();
+
+                // Set the image source to the memory stream
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = ms;
+                image.EndInit();
+
+               
+                return image;
+            }
+        }
+
+    }
+}
